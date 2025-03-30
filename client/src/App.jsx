e0982ee54b18b33e9733b01dc3ac33176d9c0d48
peveylun <cobzikl@gmail.com>
2025-03-30 18:16:30 +0300

    some stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Footer from "./components/Footer/footer.jsx";
+import Dashboard from "./components/Dashboard/dashboard.jsx";
 
 function App() {
     return (
         <>
             <Footer></Footer>
+            <Dashboard></Dashboard>
         </>
     )
 }
---
+++ client/src/components/Dashboard/dashboard.jsx
@@ -0,0 +1,12 @@
+import './dashboard.css'
+
+function Dashboard(props) {
+
+    return (
+        <div className="dashboard">
+
+        </div>
+    )
+}
+
+export default Dashboard;
