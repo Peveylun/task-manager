0af09346eac32fa0ca70c3acda1eef05aca09919
peveylun <cobzikl@gmail.com>
2025-04-15 00:20:28 +0300

    added get tasks function

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -6,7 +6,19 @@ function add_task(db, task) {
     db.run("INSERT INTO tasks AS (task_type, author_id, description) VALUES (${task.task_type}, ${task.author_id}, ${task.description});");
 }
 
+function get_tasks(db) {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM tasks;", (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        });
+    });
+}
+
 export default {
     create_table: create_table,
     add_task: add_task,
+    get_tasks: get_tasks,
 }
