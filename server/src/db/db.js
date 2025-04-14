aa66a8c4e311fdb6a247b35745ec93866fbf6a8b
peveylun <cobzikl@gmail.com>
2025-04-14 23:17:25 +0300

    added add task function

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,7 +1,12 @@
 function create_table(db) {
-    db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, author_id INTEGER NOT NULL, description TEXT NOT NULL);");
+    db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task_type TEXT NOT NULL, author_id INTEGER NOT NULL, description TEXT NOT NULL);");
+}
+
+function add_task(db, task) {
+    db.run("INSERT INTO tasks AS (task_type, author_id, description) VALUES (${task.task_type}, ${task.author_id}, ${task.description});");
 }
 
 export default {
     create_table: create_table,
+    add_task: add_task,
 }
